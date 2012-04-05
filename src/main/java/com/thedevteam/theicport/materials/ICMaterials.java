7845bd97b20bfc7b1aebe1ff5b25e7d183ae4b39
dvdbrander <dvdbrander@gmail.com>
2012-04-05 12:00:51 +0200

    Removed core dependency

--- src/main/java/com/thedevteam/theicport/materials/ICMaterials.java
+++ src/main/java/com/thedevteam/theicport/materials/ICMaterials.java
@@ -1,23 +1,28 @@
-
 package com.thedevteam.theicport.materials;
 
 //Equivilent to Vanilla's VanillaMaterials - All Blocks should be registered here
 
-import com.thedevteam.thecore.materials.Materials;
+import org.spout.api.material.Material;
 import org.spout.vanilla.material.block.Ore;
 import org.spout.vanilla.material.generic.GenericItem;
 
-public final class ICMaterials extends Materials{
-    private static boolean initialized = false;
-    // Blocks
-    public static final Ore URANIUM_ORE = register(new Ore("Uranium Ore",1));
-    
-    // Items
-    public static final GenericItem URANIUM = register(new GenericItem("Uranium", 2));
-    
-    public static void initialize(){
-        if(initialized) return;
-        URANIUM_ORE.setDrop(URANIUM);
-        initialized = true;
-    }
+public final class ICMaterials extends Material {
+	public ICMaterials(String name, int typeId) {
+		super(name, typeId);
+	}
+
+	private static boolean initialized = false;
+	// Blocks
+	public static final Ore URANIUM_ORE = register(new Ore("Uranium Ore", 1));
+
+	// Items
+	public static final GenericItem URANIUM = register(new GenericItem(
+			"Uranium", 2));
+
+	public static void initialize() {
+		if (initialized)
+			return;
+		URANIUM_ORE.setDrop(URANIUM);
+		initialized = true;
+	}
 }
