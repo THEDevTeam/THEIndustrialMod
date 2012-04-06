12a835f5bee66b0b60d096349356d58663c6ac73
Jack Huey <kitskub@yahoo.com>
2012-04-05 22:40:47 -0500

    Grinder & Macerator

---
+++ src/main/java/com/thedevteam/theindustrialmod/materials/blocks/Grinder.java
@@ -0,0 +1,15 @@
+package com.thedevteam.theindustrialmod.materials.blocks;
+
+import org.spout.vanilla.material.generic.GenericBlock;
+
+/**
+ *
+ *
+ */
+public class Grinder extends GenericBlock {
+
+    public Grinder(String name, int id) {
+        super(name, id);
+    }
+    
+}
---
+++ src/main/java/com/thedevteam/theindustrialmod/materials/blocks/Macerator.java
@@ -0,0 +1,15 @@
+package com.thedevteam.theindustrialmod.materials.blocks;
+
+import org.spout.vanilla.material.generic.GenericBlock;
+
+/**
+ *
+ *
+ */
+public class Macerator extends GenericBlock {
+
+    public Macerator(String name, int id) {
+        super(name, id);
+    }
+    
+}
