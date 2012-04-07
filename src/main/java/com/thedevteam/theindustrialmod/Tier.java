4dbdf12ccbaefd401cdc858e8f578c22c7cf4335
Jack Huey <kitskub@yahoo.com>
2012-04-07 16:50:06 -0500

    Rough sketch of ideas for tier

---
+++ src/main/java/com/thedevteam/theindustrialmod/Tier.java
@@ -0,0 +1,31 @@
+package com.thedevteam.theindustrialmod;
+
+import com.thedevteam.thetiermod.web.WebTier;
+import org.spout.api.event.EventHandler;
+import org.spout.api.event.Listener;
+import org.spout.api.event.Order;
+import org.spout.api.event.block.BlockChangeEvent;
+import org.spout.api.event.inventory.PlayerInventoryCraftEvent;
+
+/**
+ *
+ *
+ */
+public class Tier extends WebTier implements Listener{
+
+    @EventHandler(order = Order.MONITOR)
+    public void onBlockPlace(BlockChangeEvent event){
+        if(event.isCancelled()) return;
+        if (getValue(event.getSnapshot().getMaterial()) < 1) {
+            addValue(event.getSnapshot().getMaterial(), .25);
+        }
+    }
+    
+    @EventHandler(order = Order.MONITOR)
+    public void onCraftEvent(PlayerInventoryCraftEvent event){
+        if(event.isCancelled()) return;
+        if (getValue(event.getResult().getMaterial()) < 1) {
+            addValue(event.getResult().getMaterial(), .1);
+        }
+    }
+}
