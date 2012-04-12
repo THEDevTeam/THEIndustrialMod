c62808f9ff11580369ec66fa90a5ca47dda0c417
Jack Huey <kitskub@yahoo.com>
2012-04-11 22:14:18 -0500

    Tier stuff

--- src/main/java/com/thedevteam/theindustrialmod/THEIndustrialMod.java
+++ src/main/java/com/thedevteam/theindustrialmod/THEIndustrialMod.java
@@ -7,17 +7,22 @@ import org.spout.api.plugin.CommonPlugin;
 import com.thedevteam.theindustrialmod.materials.ICMaterials;
 
 public class THEIndustrialMod extends CommonPlugin{
+    TierManager tierManager;
+    private boolean usingTiers;// TODO replace with config
 
-	@Override
-	public void onDisable() {
-            // TODO Auto-generated method stub
-	}
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void onEnable() {
-		ICMaterials.initialize();
-		getLogger().log(Level.INFO, "THEIndustrialMod has been enabled");
-	}
+    @Override
+    public void onEnable() {
+        ICMaterials.initialize();
+        if(usingTiers){
+            tierManager = new TierManager();
+        }
+        getLogger().log(Level.INFO, "THEIndustrialMod has been enabled");
+    }
 
 	public void log(Level warning, Object string) {
 		// TODO Auto-generated method stub
--- src/main/java/com/thedevteam/theindustrialmod/TierManager.java
+++ src/main/java/com/thedevteam/theindustrialmod/TierManager.java
@@ -2,6 +2,7 @@ package com.thedevteam.theindustrialmod;
 
 import com.thedevteam.thecore.tier.web.WebTier;
 import org.spout.api.event.EventHandler;
+import org.spout.api.event.Listener;
 import org.spout.api.event.Order;
 import org.spout.api.event.block.BlockChangeEvent;
 import org.spout.api.event.inventory.PlayerInventoryCraftEvent;
@@ -10,13 +11,20 @@ import org.spout.api.event.inventory.PlayerInventoryCraftEvent;
  *
  *
  */
-public class Tier extends WebTier{
+public class TierManager extends WebTier implements Listener{
+    private int neededPlaceCount = 10;
+    private int neededCraftCount = 4;
+
+    public TierManager() {
+        super();
+        // TODO as materials get put in, add groups and stuff here
+    }
 
     @EventHandler(order = Order.MONITOR)
     public void onBlockPlace(BlockChangeEvent event){
         if(event.isCancelled()) return;
         if (getValue(event.getSnapshot().getMaterial()) < 1) {
-            addValue(event.getSnapshot().getMaterial(), .25);
+            addValue(event.getSnapshot().getMaterial(), 1/neededPlaceCount);
         }
     }
     
@@ -24,7 +32,7 @@ public class Tier extends WebTier{
     public void onCraftEvent(PlayerInventoryCraftEvent event){
         if(event.isCancelled()) return;
         if (getValue(event.getResult().getMaterial()) < 1) {
-            addValue(event.getResult().getMaterial(), .1);
+            addValue(event.getResult().getMaterial(), 1/neededCraftCount);
         }
     }
 }
